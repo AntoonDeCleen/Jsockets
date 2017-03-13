23b5419ddb4d21b891741ae63ba32eed8de08719
Toon <Toon@192.168.0.242>
2017-03-13 17:32:51 +0100

    /

--- Jsockets/src/client/CommandExecutor.java
+++ Jsockets/src/client/CommandExecutor.java
@@ -7,13 +7,14 @@ import java.io.PrintWriter;
 import java.net.Socket;
 import java.net.UnknownHostException;
 import java.util.Scanner;
-
+import java.util.Timer;
 
 public class CommandExecutor{
 	Command command;
 	Socket socket;
 	PrintWriter socket_out;
     BufferedReader socket_in;
+    int nullcount = 0;
 	public CommandExecutor(Command command, String URI, int sockNumber) throws IOException{
 		this.command = command;
 		try {
@@ -30,25 +31,37 @@ public class CommandExecutor{
 		switch(command){
 		case GET: 
 			System.out.println("Resource to get: ");
+			Scanner sc	= new Scanner(System.in);
+			String resource = sc.nextLine();
+			
+			 socket_out.println("GET /"+resource+" HTTP/1.1\r\n\r\n");
+			 
+			 for (String line = socket_in.readLine(); nullcount != 2; line = socket_in.readLine()) {
+				 if (nullcount == 2){
+					 break;
+				 }
+				 if (line.length() == 0){
+					 nullcount += 1;
+				 }
+			        System.out.println(line);
+			 }
+			 socket.close();
+			 System.exit(0);
+		;
+		case HEAD: 			
+			System.out.println("Resource to get: ");
 			//Scanner sc	= new Scanner(System.in);
 			//String resource = sc.nextLine();
 			//System.out.println(resource);
-			
-			 String message = "GET / HTTP/1.1\r\n\r\n";
-			 socket_out.println(message);
-			 
-//			 String response;
-//			    while (socket_in.readLine() != null) 
-//			    {
-//			        System.out.println(socket_in.readLine());
-//			    }
-			    
-			 for (String line = socket_in.readLine(); line != null; line = socket_in.readLine()) {
-			        System.out.println(line);
-			 }
-			 System.exit(0);
+
+			socket_out.println("HEAD / HTTP/1.1\r\n\r\n");
+
+			for (String line = socket_in.readLine(); line.length() != 0; line = socket_in.readLine()) {
+				System.out.println(line);
+			}
+			socket.close();
+			System.exit(0);
 		;
-		case HEAD: ;
 		case PUT:;
 		case POST:;
 		}
